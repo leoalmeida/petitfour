9d1b8c9fd4b76e69867b73ba0a61d51f0aeec73c
Leonardo Almeida silva ferreira <leoalmeida.rj@gmail.com>
2016-04-29 13:32:23 -0300

    deploy

--- deploy.cmd
+++ deploy.cmd
@@ -123,6 +123,13 @@ IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
   popd
 )
 
+:: 6. Run gulp transformations
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\gulp
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
